c8d13d2b95a9ab945ffcf28422222e6ae0025d6a
Amir <Amir@Amirs-MacBook-Pro.local>
2018-03-03 11:29:48 +0300

    Fixed ERC20Tokens

--- contracts/ERC20Exchange.sol
+++ contracts/ERC20Exchange.sol
@@ -5,7 +5,7 @@ import {StandardToken as ERC20} from "./StandardToken.sol";
 contract ERC20Exchange {
 
     mapping (bytes32 => bool) public fills;
-    
+
     // Event to log filled orders
     event Filled(address indexed makerAddress, uint makerAmount, address indexed makerToken, address takerAddress, uint takerAmount, address indexed takerToken, uint256 expiration, uint256 nonce);
     // Even to log caneled orders
@@ -17,7 +17,7 @@ contract ERC20Exchange {
     function fill(address makerAddress, uint makerAmount, address makerToken,
                   address takerAddress, uint takerAmount, address takerToken,
                   uint256 expiration, uint256 nonce, uint8 v, bytes32 r, bytes32 s) payable {
-                      
+
         // The makeAddress and takerAddress must be different
         if (makerAddress == takerAddress) {
             msg.sender.transfer(msg.value);
@@ -30,23 +30,23 @@ contract ERC20Exchange {
             Failed(2, makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce);
             return;
         }
-        
+
         bytes32 hash = validate(makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce, v, r, s);
-        
+
         // This order has already been filled
         if (fills[hash]) {
             msg.sender.transfer(msg.value);
             Failed(3, makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce);
             return;
         }
-        
+
         if (takerToken == address(0x0)) {
             if (msg.value == takerAmount) {
                 fills[hash] = true;
                 assert(transfer(makerAddress, takerAddress, makerAmount, makerToken));
                 makerAddress.transfer(msg.value);
                 Filled(makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce);
-            } 
+            }
             // The ether sent with this transaction does not match takerAmount
             else {
                 msg.sender.transfer(msg.value);
@@ -60,12 +60,12 @@ contract ERC20Exchange {
                 Failed(5, makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce);
                 return;
             }
-            
+
             if (takerAddress == msg.sender) {
                 fills[hash] = true;
                 assert(trade(makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken));
                 Filled(makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce);
-            } 
+            }
             else {
                 // The sender of this transaction must match the takerAddress
                 Failed(6, makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce);
@@ -76,21 +76,21 @@ contract ERC20Exchange {
     function cancel(address makerAddress, uint makerAmount, address makerToken,
                     address takerAddress, uint takerAmount, address takerToken,
                     uint256 expiration, uint256 nonce, uint8 v, bytes32 r, bytes32 s) {
-                        
+
         bytes32 hash = validate(makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce, v, r, s);
-        
+
         if (msg.sender == makerAddress) {
             if (fills[hash] == false) {
                 fills[hash] = true;
                 Canceled(makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce);
-            } 
+            }
             else {
                 // Order has already been cancelled or filled
                 Failed(7, makerAddress, makerAmount, makerToken, takerAddress, takerAmount, takerToken, expiration, nonce);
             }
         }
     }
-    
+
     // Function to swap tokens between two parties
     function trade(address makerAddress, uint makerAmount, address makerToken,
                    address takerAddress, uint takerAmount, address takerToken) private returns (bool) {
@@ -102,7 +102,7 @@ contract ERC20Exchange {
         require(ERC20(token).transferFrom(from, to, amount));
         return true;
     }
-    
+
     // Function to validate order arguments
     function validate(address makerAddress, uint makerAmount, address makerToken,
                       address takerAddress, uint takerAmount, address takerToken,
